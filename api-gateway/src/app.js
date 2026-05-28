ebecebbb4bedd81f5482d5b68eb11bdb731a18e4
joaoguiilhermeJ <juaohehehe528@gmail.com>
2026-05-28 20:44:31 -0300

    Correção de url em app.js

--- api-gateway/src/app.js
+++ api-gateway/src/app.js
@@ -13,7 +13,7 @@ app.set('trust proxy', 1);
 
 const corsOptions = {
   origin: [
-    "https://gerenciamendo-pedidos-frontend-9btj03oty.vercel.app",
+    "https://gerenciamendo-pedidos-frontend.vercel.app/",
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "x-api-key"],
